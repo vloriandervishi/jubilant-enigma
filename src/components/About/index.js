681be30d6f42acd68e78d04c0036bd96885fb8a4
Valdo <vlorian22@GMAIL.COM>
2021-03-05 19:01:02 -0600

    Created About component added content to index.js

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import coverImage from "../../assets/cover/cover-image.jpg";
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about"> Who am I </h1>
+      <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      />
+    </section>
+  );
+}
+export default About;
